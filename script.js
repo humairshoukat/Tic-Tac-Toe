6bf14a30826f0f177bca0f78b02ee7da60ea8ed8
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2022-04-06 21:45:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+flag = 0;
+
+function f1() {
+	if (flag == 0) {
+		document.getElementById("box1").value = "X";
+	    document.getElementById("box1").disabled = true;
+		flag = 1;
+	}
+else {
+		document.getElementById("box1").value = "0";
+		document.getElementById("box1").disabled = true;
+		flag = 0;
+	}
+}
+
+function f2() {
+	if (flag == 0) {
+		document.getElementById("box2").value = "X";
+		document.getElementById("box2").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box2").value = "0";
+		document.getElementById("box2").disabled = true;
+		flag = 0;
+	}
+}
+
+function f3() {
+	if (flag == 0) {
+		document.getElementById("box3").value = "X";
+		document.getElementById("box3").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box3").value = "0";
+		document.getElementById("box3").disabled = true;
+		flag = 0;
+	}
+}
+
+function f4() {
+	if (flag == 0) {
+		document.getElementById("box4").value = "X";
+		document.getElementById("box4").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box4").value = "0";
+		document.getElementById("box4").disabled = true;
+		flag = 0;
+	}
+}
+
+function f5() {
+	if (flag == 0) {
+		document.getElementById("box5").value = "X";
+		document.getElementById("box5").disabled = true;
+		flag = 1;
+    }
+	else {
+		document.getElementById("box5").value = "0";
+		document.getElementById("box5").disabled = true;
+		flag = 0;
+	}
+}
+
+function f6() {
+	if (flag == 0) {
+		document.getElementById("box6").value = "X";
+		document.getElementById("box6").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box6").value = "0";
+		document.getElementById("box6").disabled = true;
+		flag = 0;
+	}
+}
+
+function f7() {
+	if (flag == 0) {
+		document.getElementById("box7").value = "X";
+		document.getElementById("box7").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box7").value = "0";
+		document.getElementById("box7").disabled = true;
+		flag = 0;
+	}
+}
+
+function f8() {
+	if (flag == 0) {
+		document.getElementById("box8").value = "X";
+		document.getElementById("box8").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box8").value = "0";
+		document.getElementById("box8").disabled = true;
+		flag = 0;
+	}
+}
+
+function f9() {
+	if (flag == 0) {
+		document.getElementById("box9").value = "X";
+		document.getElementById("box9").disabled = true;
+		flag = 1;
+	}
+	else {
+		document.getElementById("box9").value = "0";
+		document.getElementById("box9").disabled = true;
+		flag = 0;
+	}
+}
+
+function f10() {
+	location.reload();
+	/*document.getElementById('box1').value = '';
+	document.getElementById("box2").value = '';
+	document.getElementById("box3").value = '';
+	document.getElementById("box4").value = '';
+	document.getElementById("box5").value = '';
+	document.getElementById("box6").value = '';
+	document.getElementById("box7").value = '';
+	document.getElementById("box8").value = '';
+	document.getElementById("box9").value = '';*/
+}
